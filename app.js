1015ff5e40f135b288990115672a35573843d6bd
neelesh.synergyworks@gmail.com <neelesh.synergyworks@gmail.com>
2020-08-06 21:47:34 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { v4: uuid } = require("uuid");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss");
+
+// security
+(function () {
+  app.use(cors());
+  app.use(xss());
+  app.use(helmet());
+})();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
+});
